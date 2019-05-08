3a1967ccaedb2317c9c14869875573d8c7c1e42a
PierreNat <pierre.cuony@students.unibe.ch>
2019-05-08 17:48:18 +0200

    type now is working, no I have to underdstand how the cross entropy will work with my images

--- pipeline/DatabaseTester.py
+++ pipeline/DatabaseTester.py
@@ -28,23 +28,33 @@ for i in range(0,10):
     param = params[i]
     print(param)
 
-    fig.add_subplot(1, 2, 1)
-    plt.imshow(img)
-
-    fig.add_subplot(1, 2, 2)
-    plt.imshow(sil, cmap='gray')
-    plt.show()
-    plt.close(fig)
+    # fig.add_subplot(1, 2, 1)
+    # plt.imshow(img)
+    #
+    # fig.add_subplot(1, 2, 2)
+    # plt.imshow(sil, cmap='gray')
+    # plt.show()
+    # plt.close(fig)
 
     loss = nn.CrossEntropyLoss()
-    input = torch.randn(10, 5, requires_grad=True) #torch float 32
-    target = torch.empty(10, dtype=torch.long).random_(5) #torch int 64
-    input = torch.from_numpy(np.array([[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]]))
-    target = torch.from_numpy(np.array([1,2,3,4,5,6,7,8])).CharTensor
+    # input = torch.randn(10, 4, requires_grad=True)  #torch float 32
+    input = (torch.from_numpy(np.array([[1, 1 , 1 , 1, 1],
+                                        [1, 1 , 1 , 1, 1],
+                                        [1, 1 , 1 , 1, 1],
+                                        [1, 1 , 1 , 1, 1],
+                                        [1, 1 , 1 , 1, 1],
+                                        [1, 1 , 1 , 1, 1]])))
+    input = input.double()
+    # target = input
+    # target = torch.empty(10, dtype=torch.long).random_(5) #torch int 64
+    target = torch.from_numpy(np.array([1, 1 , 1 , 1, 1 ,1 ])) #torch int 64
+    target = target.long()
+    # input = (torch.from_numpy(np.array([[1,2,3,4,5,6,7,8],[1,2,3,4,5,6,7,8]]))).type(torch.)
+    # target = torch.from_numpy(np.array([1,2,3,4,5,6,7,8])).CharTensor
     output = loss(input, target)
-    Sil_1d_1 = torch.from_numpy((np.reshape(sil, np.size(sil,0)*np.size(sil,1))/255)) #size, [batch = 1, class = 512*512]
+    Sil_1d_1 = torch.from_numpy((np.reshape(sil, np.size(sil,0)*np.size(sil,1))/255)).double()#size, [batch = 1, class = 512*512]
 
-    Sil_1d_2 = torch.from_numpy(np.reshape(sil2, np.size(sil2,0)*np.size(sil2,1))) #size = 512*512 values
+    Sil_1d_2 = torch.from_numpy(np.reshape(sil2, np.size(sil2,0)*np.size(sil2,1))).long() #size = 512*512 values
     Sil_1d_1 = Sil_1d_1.to(device)
     Sil_1d_2 = Sil_1d_2.to(device)
 
